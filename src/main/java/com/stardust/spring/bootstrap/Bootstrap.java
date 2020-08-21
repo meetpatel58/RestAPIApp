c78c18a06192969b35d3bc195c94a7f692bc6706
meetpatel58 <meetpatel58@ymail.com>
2020-08-21 16:12:24 -0400

    Added Bootstrap class to load data

---
+++ src/main/java/com/stardust/spring/bootstrap/Bootstrap.java
@@ -0,0 +1,40 @@
+package com.stardust.spring.bootstrap;
+
+import com.stardust.spring.model.Catagory;
+import com.stardust.spring.repositories.CatagoryRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Bootstrap implements CommandLineRunner {
+
+    private CatagoryRepository catagoryRepository;
+
+    public Bootstrap(CatagoryRepository catagoryRepository) {
+        this.catagoryRepository = catagoryRepository;
+    }
+
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        Catagory monitors = new Catagory();
+        monitors.setName("Monitors");
+
+        Catagory ram = new Catagory();
+        ram.setName("RAM");
+
+        Catagory cpu = new Catagory();
+        ram.setName("CPU");
+
+        Catagory gpu = new Catagory();
+        ram.setName("GPU");
+
+        catagoryRepository.save(monitors);
+        catagoryRepository.save(ram);
+        catagoryRepository.save(cpu);
+        catagoryRepository.save(gpu);
+
+        System.out.println("Data Loaded = " + catagoryRepository.count());
+    }
+}
